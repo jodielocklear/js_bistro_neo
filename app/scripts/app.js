c63615b47d3f3442bd214ea249b7b80ab5520401
Laura Moore <laura.moore@rightbox.com>
2013-03-08 19:31:23 -0500

    initial yeoman build plus starter files

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var jsBistroNeoApp = angular.module('jsBistroNeoApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+jsBistroNeoApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
